7dc009b0c8d2967761ba45d1f2ca8e47c11aa31e
Manasseh Gitau <kimmanasseh14@gmail.com>
2023-05-05 23:30:43 +0300

    add connected vehicles

---
+++ Transport/app.py
@@ -0,0 +1,31 @@
+from counterfit_connection import CounterFitConnection
+CounterFitConnection.init('127.0.0.1', 5000)
+
+import time 
+import counterfit_shims_serial
+import pynmea2
+
+serial = counterfit_shims_serial.Serial("/dev/ttyAMA0")
+
+def print_gps_data(line):
+    msg = pynmea2.parse(line)
+    if msg.sentence_type == 'GGA':
+        lat = pynmea2.dm_to_sd(msg.lat)
+        lon = pynmea2.dm_to_sd(msg.lon)
+
+        if msg.lat_dir == 'S':
+            lat *= -1
+
+        if msg.lon_dir == 'W':
+            lon *= -1
+
+        print(f'{lat}, {lon} -from {msg.num_sats} satellites')
+    
+while True:
+    line = serial.readline().decode('utf-8')
+
+    while len(line) > 0:
+        print_gps_data(line)
+        line = serial.readline().decode('utf-8')
+
+    time.sleep(1)
