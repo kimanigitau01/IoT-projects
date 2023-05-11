816fb4e74d68dd7dce12f5ebe8a27eae97dc1c3a
Manasseh Gitau <110011157+kimanigitau01@users.noreply.github.com>
2023-05-11 23:10:24 +0300

    add files for gps-sensor updated

--- gps-sensor/app.py
+++ gps-sensor/app.py
@@ -1,23 +1,23 @@
 from counterfit_connection import CounterFitConnection
 CounterFitConnection.init('127.0.0.1', 5000)
 
-import time
-import counterfit_shims_serial
-import pynmea2
+import time 
 import json
+import counterfit_shims_serial
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
-connection_string = 'HostName=gps-sensor.azure-devices.net;DeviceId=gps-sensor1;SharedAccessKey=37hOdrMrFELRlHZNCGWZwkuWUlysgX0PpPuUM6AweSE='
+serial = counterfit_shims_serial.Serial('/dev/ttyAMA0')
+connection_string = "HostName=gps-sensor-1.azure-devices.net;DeviceId=gps-sensor;SharedAccessKey=VqetOmzZD4u/UO2dI0MVGEyVK7JCwuzzdbhV5rSNpuc="
 
 device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 
-print('Connecting')
+print('connecting')
 device_client.connect()
 print('Connected')
 
-serial = counterfit_shims_serial.Serial('/dev/ttyAMA0')
+import pynmea2
 
-def send_gps_data(line):
+def print_gps_data(line):
     msg = pynmea2.parse(line)
     if msg.sentence_type == 'GGA':
         lat = pynmea2.dm_to_sd(msg.lat)
@@ -25,21 +25,18 @@ def send_gps_data(line):
 
         if msg.lat_dir == 'S':
             lat = lat * -1
-
-        if msg.lon_dir == 'W':
+        if msg.lon_dir == "W":
             lon = lon * -1
 
-        # print(f'{lat},{lon} - from {msg.num_sats} satellites')
-        message_json = { "gps" : { "lat":lat, "lon":lon } }
-        print("Sending telemetry", message_json)
-        message = Message(json.dumps(message_json))
+        message = Message(json.dumps({'gps': {'lat':lat, 'lon': lon, 'num of sats': msg.num_sats}}))
+        device_client.send_message(message)
+        print(f'sending telemetry: ', {lat},{lon},{msg.num_sats})
 
 while True:
-    line = serial.readline().decode('utf-8')
+    line =serial.readline().decode('utf-8')
 
     while len(line) > 0:
-        send_gps_data(line)
-        line = serial.readline().decode('utf-8') 
+        print_gps_data(line)
+        line = serial.readline().decode('utf-8')
 
     time.sleep(60)
-    
