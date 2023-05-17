8e16d6da1a59ecb4a6e2a451d03bb42c504c6f11
Manasseh Gitau <110011157+kimanigitau01@users.noreply.github.com>
2023-05-17 20:19:06 +0300

    add iot edge

--- Manufacturing/fruit-quality-detector/app.py
+++ Manufacturing/fruit-quality-detector/app.py
@@ -3,9 +3,7 @@ CounterFitConnection.init('127.0.0.1', 5000)
 
 import io
 from counterfit_shims_picamera import PiCamera
-
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from msrest.authentication import ApiKeyCredentials
+import requests
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -18,19 +16,13 @@ image.seek(0)
 with open('image.jpg', 'wb') as image_file:
     image_file.write(image.read())
 
-prediction_url = 'https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/9b5886e0-5454-469a-b229-f2e46e093f82/classify/iterations/Iteration2/image'
-prediction_key = 'Loading...'
-
-parts = prediction_url.split('/')
-endpoint = 'https://' + parts[2]
-project_id = parts[6]
-iteration_name = parts[9]
-
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-predictor = CustomVisionPredictionClient(endpoint, prediction_credentials)
-
+prediction_url = 'http://192.168.137.251/image' # Replace with your IP address
+headers = {
+    'Content-Type' : 'application/octet-stream'
+}
 image.seek(0)
-results = predictor.classify_image(project_id, iteration_name, image)
+response = requests.post(prediction_url, headers=headers, data=image)
+results = response.json()
 
-for prediction in results.predictions:
-    print(f'{prediction.tag_name}:\t{prediction.probability * 100:.2f}%')
+for prediction in results['predictions']:
+    print(f'{prediction["tagName"]}:\t{prediction["probability"] * 100:.2f}%')
